353cf31cd3e285719f56994dab77af524917fccf
Eresedia Chademuwiri <chademuwirie@icloud.com>
2024-03-22 03:36:08 +0200

    d

--- src/config/db/index.ts
+++ src/config/db/index.ts
@@ -3,6 +3,9 @@ import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as schema from "./schema"
 
+import dotenv from "dotenv"
+dotenv.config()
+
 const client = process.env.ENV === "prod" ? createClient({ url: process.env.DB_URL, authToken: process.env.DB_AUTH_TOKEN })
   : createClient({
     url: 'file:./.data/local.sqlite'
